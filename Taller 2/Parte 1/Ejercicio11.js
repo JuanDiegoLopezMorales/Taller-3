2942b940f52fa28ac0a712c32ccb51fafe7c9cf1
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2024-09-02 18:27:55 -0500

    Update Ejercicio11.js

--- Taller 2/Parte 1/Ejercicio11.js
+++ Taller 2/Parte 1/Ejercicio11.js
@@ -2,9 +2,9 @@ let x = 2
 let y = 3
 
 switch(x){
-    case 2: console.log(x+4)
+    case "2": console.log(x+4)
     break
-    case 3: console.log(x+3)
+    case "3": console.log(x+3)
     break
     case "dos": console.log(y+x)
     break
